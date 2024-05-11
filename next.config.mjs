47d24f4776d8c9502b73a3eb8d644ff84a90a27c
gilang-nugraha <128233782+gilang-nugraha@users.noreply.github.com>
2024-05-12 05:58:07 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "timesheet-demo.gilanglie.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
